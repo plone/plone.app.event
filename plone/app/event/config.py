e43ecbf61164778a1e55e2883f97935648d91248
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-15 15:21:41 +0000

    atct config
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34800 3b4cdb85-528f-c531-b63d-5919d5b1aa08

---
+++ plone/app/event/config.py
@@ -0,0 +1,10 @@
+PROJECTNAME = "plone.app.event"
+
+ADD_PERMISSIONS = {
+    'Event': 'plone.app.event: Add Folderish Event',
+}
+
+# from Products.CMFCore.permissions import setDefaultRoles
+# DEFAULT_ADD_CONTENT_PERMISSION = "Add portal content"
+# setDefaultRoles(DEFAULT_ADD_CONTENT_PERMISSION,
+#                 ('Manager', 'Owner', 'Contributor'))
