f80eb0b14bb366c3d2e9ff8f7b49de75e322aa3f
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-15 15:22:03 +0000

    atct initialization
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34801 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/__init__.py
+++ plone/app/event/__init__.py
@@ -1,1 +1,27 @@
-#
+from zope.i18nmessageid import MessageFactory
+from plone.app.event import config
+
+if HAS_LINGUA_PLONE:
+    from Products.LinguaPlone import public as atapi
+else:
+    from Products.Archetypes import atapi
+
+from Products.CMFCore import utils
+
+MsgFact = MessageFactory(config.PROJECTNAME)
+
+def initialize(context):
+    """Register content types through Archetypes with Zope and the CMF.
+    """
+    from content import event
+
+    content_types, constructors, ftis = atapi.process_types(
+        atapi.listTypes(config.PROJECTNAME),
+        config.PROJECTNAME)
+
+    for atype, constructor in zip(content_types, constructors):
+        utils.ContentInit("%s: %s" % (config.PROJECTNAME, atype.portal_type),
+            content_types      = (atype,),
+            permission         = config.ADD_PERMISSIONS[atype.portal_type],
+            extra_constructors = (constructor,),
+            ).initialize(context)
