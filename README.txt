6cf17b2403b7ff4c237ed728ffd47dfb6e5a55a5
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-15 14:01:29 +0000

    initial from paster
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34774 3b4cdb85-528f-c531-b63d-5919d5b1aa08

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This package provides an event content type for plone based on Archetypes.
+
+It replaces the ATContentType event impementation.
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plone.app.event',
+      version=version,
+      description="Event content type for plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone event',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='/svn/plone/plone.app.event/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone','plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      )
