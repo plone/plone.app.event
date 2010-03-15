adf1d7f8b84c019763cf389904e185ac166a921d
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-15 17:48:19 +0000

    formating
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34871 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/interfaces.py
+++ plone/app/event/interfaces.py
@@ -3,7 +3,7 @@ from Products.ATContentTypes.interfaces.interfaces import IATContentType
 
 class IATEvent(IATContentType):
     """AT Event marker interface
-     """
+    """
 
 class ICalendarSupport(Interface):
     """Calendar import/export
