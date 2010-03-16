51165d4c9e012b91582ff5b495b9f5cafe64f1a5
ajung <ajung@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-16 12:48:30 +0000

    greatly cleaned up the event_view.pt mess and added support for whole-day and not-end-date fields
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34928 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,10 +1,12 @@
 from Products.Five.browser import BrowserView
 
 
+from plone.app.event import event_util
+
 class EventView(BrowserView):
 
+    def date_for_display(self):
 
-    pass
-
-
+        # ATT: format string
+        return event_util.toDisplay(self.context)
 
