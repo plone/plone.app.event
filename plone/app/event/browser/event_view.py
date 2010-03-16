cc440e34d106859c8736db70cb37189cd3ad743d
ajung <ajung@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-16 13:16:10 +0000

    removed ATT
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@34939 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,12 +1,9 @@
 from Products.Five.browser import BrowserView
 
-
 from plone.app.event import event_util
 
 class EventView(BrowserView):
 
     def date_for_display(self):
-
-        # ATT: format string
         return event_util.toDisplay(self.context)
 
