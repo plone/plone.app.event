71b683a60e7304988666b9112af22e9e0655090f
ajung <ajung@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-17 15:34:05 +0000

    fixed import
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@35137 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/event_util.py
+++ plone/app/event/event_util.py
@@ -3,6 +3,7 @@ Display helper methods for showing whole-day or same-day events
 """
 
 from Products.CMFPlone.i18nl10n import ulocalized_time
+from plone.app.event.utils import rfc2445dt
 
 def isSameDay(event):
 
