a001d8c254e5ab28464dd92444449554f801c27d
vincentfretin <vincentfretin@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-18 17:12:06 +0000

    Fixed tests in test_event_utils .py
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@35316 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/tests/test_event_utils.py
+++ plone/app/event/tests/test_event_utils.py
@@ -8,14 +8,13 @@ from plone.app.event.tests.base import EventTestCase
 
 class EventUtilsTests(EventTestCase):
 
-    def _makeOne(self, start, end, useEndDate=True, wholeDay=False):
+    def _makeOne(self, start, end, wholeDay=False):
         self.login()
         self.setRoles('Manager')
         self.portal.invokeFactory('Event', id='event')
         event = self.portal['event']
         event.setStartDate(start)
         event.setEndDate(end)
-        event.setUseEndDate(useEndDate)
         event.setWholeDay(wholeDay)
         return event
 
@@ -27,24 +26,13 @@ class EventUtilsTests(EventTestCase):
         event = self._makeOne('2000/10/12 06:00:00', '2000/10/13 18:00:00')
         self.assertEqual(event_util.isSameDay(event), False)
 
-    def testIsSameDayWithoutEndDate(self):
-        # events with useEndDate==False are always same-day events
-        event = self._makeOne('2000/10/12 06:00:00', '2000/10/12 18:00:00', 
-                          useEndDate=False)
-        self.assertEqual(event_util.isSameDay(event), True)
-
-    def testIsSameDayWithoutEndDateFailing(self):
-        event = self._makeOne('2000/10/12 06:00:00', '2000/10/13 18:00:00', 
-                          useEndDate=False)
-        self.assertEqual(event_util.isSameDay(event), True)
-
     def testToDisplayWithTime(self):
         event = self._makeOne('2000/10/12 06:00:00', '2000/10/12 18:00:00')
         self.assertEqual(event_util.toDisplay(event), 
                 {'start_date': 'Oct 12, 2000',
-                 'start_time' : '07:00 AM',
+                 'start_time' : '06:00 AM',
                  'end_date' : 'Oct 12, 2000',
-                 'end_time' : '07:00 PM',
+                 'end_time' : '06:00 PM',
                  'same_day' : True,
                 })
 
@@ -70,34 +58,8 @@ class EventUtilsTests(EventTestCase):
                  'same_day' : False,
                 })
 
-    def testToDisplayWithoutEndDateDifferentDates(self):
-        event = self._makeOne('2000/10/12 06:00:00', '2000/10/13 18:00:00', 
-                          useEndDate=False,
-                          wholeDay=True)
-        self.assertEqual(event_util.toDisplay(event), 
-                {'start_date': 'Oct 12, 2000',
-                 'start_time': None, 
-                 'end_date' : 'Oct 13, 2000',
-                 'end_time': None, 
-                 'same_day' : True,
-                })
-
-    def testToDisplayWithoutEndDateStartAndEndDateEqual(self):
-        event = self._makeOne('2000/10/12 06:00:00', '2000/10/12 18:00:00', 
-                          useEndDate=False,
-                          wholeDay=True)
-        self.assertEqual(event_util.toDisplay(event), 
-                {'start_date': 'Oct 12, 2000',
-                 'start_time' : None,
-                 'end_date' : 'Oct 12, 2000',
-                 'end_time' : None,
-                 'same_day' : True,
-                })
-
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(EventUtilsTests))
     return suite
-
-
