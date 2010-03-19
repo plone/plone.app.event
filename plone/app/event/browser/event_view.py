8cea8ca9d856dd9cf4a8869aee8d9e8b33e97498
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-19 19:30:36 +0000

    get_occurences of event, based on calculating recrule. can't ask index since values are stored in internal representation there
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@35404 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,9 +1,51 @@
 from Products.Five.browser import BrowserView
-
+from Products.CMFPlone.i18nl10n import ulocalized_time
 from plone.app.event import event_util
+
+from Products.DateRecurringIndex.recurring import RRuleICal
+from Products.DateRecurringIndex.interfaces import IRecurringSequence
 
 class EventView(BrowserView):
 
     def date_for_display(self):
         return event_util.toDisplay(self.context)
 
+    def get_occurences(self):
+        from Products.CMFCore.utils import getToolByName
+        cat = getToolByName(self, 'portal_catalog')
+
+        delta = [eval(rr) for rr in getattr(self.context, 'recurrence', None) if isinstance(rr,str) or rr]
+        starts = IRecurringSequence(RRuleICal(self.context.start(), delta))
+        ends = IRecurringSequence(RRuleICal(self.context.end(), delta))
+
+        events = map(
+            lambda start,end:dict(
+                start_date = ulocalized_time(start, False, time_only=None, context=self.context),
+                end_date = ulocalized_time(end, False, time_only=None, context=self.context),
+                start_time = ulocalized_time(start, False, time_only=True, context=self.context),
+                end_time = ulocalized_time(end, False, time_only=True, context=self.context),
+                same_day = event_util.isSameDay(self.context),
+                same_time = event_util.isSameTime(self.context),
+            ), starts, ends )
+
+        """
+        # TODO: don't ask the index for occurences, calculate them from recrule
+        # index is integer representation which cannot (really?) be reversed
+        # see Products.DateRecurringIndex.utils.dt2int
+        events = None
+        cat_item = cat.searchResults(**{'UID':self.context.UID()})
+        if cat_item:
+            events = map(
+                lambda start,end:dict(
+                    start_date = ulocalized_time(start, False, time_only=None, context=self.context),
+                    end_date = ulocalized_time(end, False, time_only=None, context=self.context),
+                    start_time = ulocalized_time(start, False, time_only=True, context=self.context),
+                    end_time = ulocalized_time(end, False, time_only=True, context=self.context),
+                    same_day = event_util.isSameDay(self.context),
+                    same_time = event_util.isSameTime(self.context),
+                ),
+                cat.getIndexDataForRID(cat_item[0].getRID())['start'],
+                cat.getIndexDataForRID(cat_item[0].getRID())['end']
+                )
+        """
+        return events
