c6ff6f33008ba57af29733a36928349f8f59aac4
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-03-23 09:30:11 +0000

    still working on to get DateRecurringIndex installed before plone.app.event's profile is applied without success
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@35500 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/tests/base.py
+++ plone/app/event/tests/base.py
@@ -1,22 +1,29 @@
-from Products.Five.zcml import load_config
+from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
+#from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase.layer import onsetup
 from Products.ATContentTypes.tests import atcttestcase, atctftestcase
 
 @onsetup
 def setupPackage():
     fiveconfigure.debug_mode = True
-    import Products.DateRecurringIndex
-    load_config('configure.zcml', Products.DateRecurringIndex)
-    ptc.installPackage('Products.DateRecurringIndex')
     import plone.app.event
-    load_config('configure.zcml', plone.app.event)
-    ptc.installPackage('plone.app.event')
+    zcml.load_config('configure.zcml', plone.app.event)
+    import Products.Five
+    zcml.load_config('meta.zcml', Products.Five)
+    zcml.load_config('configure.zcml', Products.Five)
+    import Products.DateRecurringIndex
+    zcml.load_config('configure.zcml', Products.DateRecurringIndex)
+    ptc.installProduct('Products.DateRecurringIndex')
+    ptc.installProduct('plone.app.event')
     fiveconfigure.debug_mode = False
 
 setupPackage()
-ptc.setupPloneSite(extension_profiles=['plone.app.event:default'])
+
+ptc.setupPloneSite()
+# ptc.PloneTestCase.addProfile
+#ptc.setupPloneSite(extension_profiles=['plone.app.event:default'])
 
 class EventTestCase(ptc.PloneTestCase):
     """ Base class for plone.app.event tests """
