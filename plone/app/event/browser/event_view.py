e2a839f40e0160c297c28ace93df704cb0c1057a
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-09-23 20:41:22 +0000

    use adapter lookup to get recurrence occurrences
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@40178 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -2,30 +2,36 @@ from Products.Five.browser import BrowserView
 from Products.CMFPlone.i18nl10n import ulocalized_time
 from plone.app.event import event_util
 
-from Products.DateRecurringIndex.recurring import RecurConfICal
-from Products.DateRecurringIndex.interfaces import IRecurringSequence
+#from Products.DateRecurringIndex.recurring import RecurConfICal
+#from Products.DateRecurringIndex.interfaces import IRecurringSequence
+from plone.app.event.interfaces import IRecurrence
 
 class EventView(BrowserView):
 
     def date_for_display(self):
         return event_util.toDisplay(self.context)
 
-    def get_occurences(self):
-        rrules = self.context.getRecurrence()
-        if rrules is None:
-            return []
 
-        starts = IRecurringSequence(RecurConfICal(self.context.start(), rrules))
-        ends = IRecurringSequence(RecurConfICal(self.context.end(), rrules))
-        events = map(
-            lambda start,end:dict(
-                start_date = ulocalized_time(start, False, time_only=None, context=self.context),
-                end_date = ulocalized_time(end, False, time_only=None, context=self.context),
-                start_time = ulocalized_time(start, False, time_only=True, context=self.context),
-                end_time = ulocalized_time(end, False, time_only=True, context=self.context),
-                same_day = event_util.isSameDay(self.context),
-                same_time = event_util.isSameTime(self.context),
-            ), starts, ends )
+    def occurrences(self):
+        recurrence = IRecurrence(self.context)
+        return recurrence.occurences()
+
+    #def get_occurences(self):
+        #rrules = self.context.getRecurrence()
+        #if rrules is None:
+            #return []
+
+        #starts = IRecurringSequence(RecurConfICal(self.context.start(), rrules))
+        #ends = IRecurringSequence(RecurConfICal(self.context.end(), rrules))
+        #events = map(
+            #lambda start,end:dict(
+                #start_date = ulocalized_time(start, False, time_only=None, context=self.context),
+                #end_date = ulocalized_time(end, False, time_only=None, context=self.context),
+                #start_time = ulocalized_time(start, False, time_only=True, context=self.context),
+                #end_time = ulocalized_time(end, False, time_only=True, context=self.context),
+                #same_day = event_util.isSameDay(self.context),
+                #same_time = event_util.isSameTime(self.context),
+            #), starts, ends )
 
         """
         from Products.CMFCore.utils import getToolByName
@@ -46,4 +52,4 @@ class EventView(BrowserView):
                 cat.getIndexDataForRID(cat_item[0].getRID())['end']
                 )
         """
-        return events
+        #return events
