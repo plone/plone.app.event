1cd26ad28d5b7721198a5928764e8741cec45f24
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-09-23 20:15:30 +0000

    add generic eventish interfaces and recurrence support interface
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@40174 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/interfaces.py
+++ plone/app/event/interfaces.py
@@ -5,3 +5,32 @@ class ICalendarSupport(zope.interface.Interface):
     """Calendar import/export
     """
 
+class IEvent(zope.interface.Interface):
+    """Generic calendar event for Plone
+    """
+
+class IRecurringEvent(zope.interface.Interface):
+    """Generic recurring calendar event for Plone
+    """
+
+class IRecurrence(zope.interface.Interface):
+    """Interface for adapter providing recurrence support
+    """
+
+    def recurrence_ruleset():
+        """Returns the recurrence ruleset as dateutil.rrule.rruleset instance.
+        """
+
+    def occurences_start():
+        """Returns all the event's start occurences which indicates the
+           beginning of each event.
+        """
+
+    def occurences_end():
+        """Returns all the event's end occurences which indicates the
+           ending of each event.
+        """
+
+    def occurences():
+        """Returns all the event's start and end occurences as a list of tuples.
+        """
