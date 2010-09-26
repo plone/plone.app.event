e19edfb7632127de1cd288b1c28c2c8cdecdff14
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2010-09-26 20:32:58 +0000

    bugfix + event stub class
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@40296 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/tests/test_doctests.py
+++ plone/app/event/tests/test_doctests.py
@@ -1,10 +1,39 @@
+import unittest
 import doctest
 from interlude import interact
+import zope.component
 
 OPTIONFLAGS = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 DOCFILES = [
     '../recurrence.txt',
 ]
+
+from zope.interface import implements
+from plone.app.event.interfaces import IRecurringEvent
+class RecurrenceStub(object):
+    """Basic stub object for testing events.
+    """
+    implements(IRecurringEvent)
+    def __init__(self, recurrence=None, start_date=None, end_date=None):
+        self.recurrence = recurrence
+        self.start_date = start_date
+        self.end_date = end_date
+        self.duration = end_date - start_date
+        #subject
+        #location
+        #wholeDay
+        #startDate
+        #endDate
+        #text
+        #attendees
+        #eventUrl
+        #contactName
+        #contactEmail
+        #contactPhone
+        #recurrence
+        #start_date
+        #end_date
+        #duration
 
 def test_suite():
     suite = unittest.TestSuite()
@@ -12,7 +41,8 @@ def test_suite():
         doctest.DocFileSuite(
             docfile,
             optionflags=OPTIONFLAGS,
-            globs={'interact': interact,}
+            globs={'interact': interact,},
+            tearDown=zope.component.testing.tearDown
         ) for docfile in DOCFILES
     ])
     return suite
