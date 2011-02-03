2499470da3ec1c2d79459e1dffb6cbada185ad89
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2011-02-03 21:56:31 +0000

    register inline doctests for timezone module in testsuite
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@47210 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/tests/test_doctests.py
+++ plone/app/event/tests/test_doctests.py
@@ -1,11 +1,22 @@
 import unittest
 import doctest
-#from interlude import interact
+from interlude import interact
 
 OPTIONFLAGS = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 DOCFILES = [
     '../recurrence.txt',
 ]
+DOCMODS = [
+    'plone.app.event.timezone',
+]
+
+from zope.configuration import xmlconfig
+import zope.component
+import plone.app.event
+def load_zcml(doctest_context):
+    context = xmlconfig.file('meta.zcml', zope.component)
+    xmlconfig.file('configure.zcml', zope.component, context=context)
+    xmlconfig.file('configure.zcml', plone.app.event, context=context)
 
 def test_suite():
     suite = unittest.TestSuite()
@@ -16,6 +27,13 @@ def test_suite():
             globs={#'interact': interact,
                 },
         ) for docfile in DOCFILES
+    ])
+    suite.addTests([
+        doctest.DocTestSuite(docmod,
+                             setUp=load_zcml,
+                             optionflags=OPTIONFLAGS,
+                             globs={'interact': interact, }
+                             ) for docmod in DOCMODS
     ])
     return suite
 
