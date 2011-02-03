b881ac857fa6ae205bfc40664cfd324e97f11459
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2011-02-03 22:05:31 +0000

    add portal timezone getter utility
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@47212 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/timezone.py
+++ plone/app/event/timezone.py
@@ -4,6 +4,33 @@ from zope.interface import directlyProvides
 from collective.elephantvocabulary import wrap_vocabulary
 from plone.registry.interfaces import IRegistry
 from plone.app.event.controlpanel.event import IEventSettings
+from plone.event.interfaces import ITimezoneGetter
+import pytz
+
+class TimezoneGetter(object):
+    """ Retrieve the timezone from the portal or user.
+
+    """
+
+    @property
+    def timezone(self):
+        """ Get the timezone.
+
+        >>> from plone.event.interfaces import ITimezoneGetter
+        >>> from zope.component import getUtility
+        >>> tzutil = getUtility(ITimezoneGetter)
+        >>> interact(locals(), use_ipython=False )
+        """
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IEventSettings)
+        timezone = settings.portal_timezones
+        # TODO: retrieve the timezone of the current user, if set.
+        if not timezone:
+            timezone = getUtility(ITimezoneGetter,
+                    'FallbackTimezoneGetter')().timezone
+        # following statement ensures, that timezone is a valid pytz zone
+        return pytz.timezone(timezone).zone
+
 
 # TODO: cache me
 def AvailableTimezonesVocabulary(context):
