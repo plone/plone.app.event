e3131475c888bc71d36a1378c48dc085d873bac0
thet <thet@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2011-02-04 23:16:55 +0000

    availabletimezone vocabulary and timezonegetter utility working and tested
    git-svn-id: https://svn.plone.org/svn/plone/plone.app.event/trunk@47251 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/app/event/timezone.py
+++ plone/app/event/timezone.py
@@ -16,14 +16,26 @@ class TimezoneGetter(object):
     def timezone(self):
         """ Get the timezone.
 
-        >>> from plone.event.interfaces import ITimezoneGetter
         >>> from zope.component import getUtility
+        >>> from plone.registry.interfaces import IRegistry
+        >>> from plone.app.event.controlpanel.event import IEventSettings
+        >>> reg = getUtility(IRegistry)
+        >>> settings = reg.forInterface(IEventSettings)
+
+        >>> from plone.event.interfaces import ITimezoneGetter
         >>> tzutil = getUtility(ITimezoneGetter)
-        >>> interact(locals(), use_ipython=False )
+        >>> tzutil().timezone
+        'CET'
+
+        >>> settings.portal_timezone = "Europe/Vienna"
+        >>> tzutil().timezone
+        'Europe/Vienna'
+
+        #>>> from interlude import interact; interact(locals(), use_ipython=False )
         """
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IEventSettings)
-        timezone = settings.portal_timezones
+        timezone = settings.portal_timezone
         # TODO: retrieve the timezone of the current user, if set.
         if not timezone:
             timezone = getUtility(ITimezoneGetter,
@@ -32,20 +44,46 @@ class TimezoneGetter(object):
         return pytz.timezone(timezone).zone
 
 
-# TODO: cache me
 def AvailableTimezonesVocabulary(context):
     """
-    >>> import zope.component
+    >>> from zope.component import getUtility
+    >>> from plone.registry.interfaces import IRegistry
+    >>> from plone.app.event.controlpanel.event import IEventSettings
+    >>> reg = getUtility(IRegistry)
+    >>> settings = reg.forInterface(IEventSettings)
+
     >>> from zope.schema.interfaces import IVocabularyFactory
-    >>> tzvocab = zope.component.getUtility(IVocabularyFactory,
+    >>> tzvocab = getUtility(IVocabularyFactory,
     ...     'AvailableTimezonesVocabulary')
-    >>> interact(locals(), use_ipython=False )
+    >>> list(tzvocab(None))
+    []
+
+    >>> allzones = getUtility(IVocabularyFactory, 'TimezoneVocabulary')(None)
+    >>> zones = [zone.value for zone in list(allzones)[:4]]
+
+    >>> settings.portal_timezone = zones[0]
+    >>> assert([term.value for term in list(tzvocab(None))] == zones[0:1])
+
+    >>> settings.available_timezones = zones[1:]
+    >>> assert([term.value for term in list(tzvocab(None))] == zones)
+
+    >>> settings.available_timezones = zones
+    >>> assert([term.value for term in list(tzvocab(None))] == zones)
+
+
+
     """
     registry = getUtility(IRegistry)
     settings = registry.forInterface(IEventSettings)
-    available = settings.available_timezones
-    if settings.portal_timezone not in available:
-        available.append(settings.portal_timezone)
-    tzvocab = getUtility(IVocabularyFactory, 'TimezoneVocabulary')
-    return wrap_vocabulary(tzvocab, visible_terms=available)
+    available = settings.available_timezones or []
+    portaltz = settings.portal_timezone
+    if portaltz and portaltz not in available: available.append(portaltz)
+    tzvocab = getUtility(IVocabularyFactory, 'TimezoneVocabulary')(context)
+    return wrap_vocabulary(tzvocab, visible_terms=available)(context)
+    # TODO: remove following block
+    #return wrap_vocabulary(
+    #    tzvocab,
+    #    visible_terms_from_registry=\
+    #        'plone.app.event.controlpanel.event.IEventSettings.available_timezones'
+    #    )(context)
 directlyProvides(AvailableTimezonesVocabulary, IVocabularyFactory)
