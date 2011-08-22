096c06d74f09e201a4e5345bbe704882674d74b9
Johannes Raggam <raggam-nl@adm.at>
2011-08-22 16:35:46 +0200

    bugfix

--- plone/app/event/__init__.py
+++ plone/app/event/__init__.py
@@ -2,7 +2,7 @@ from Products.CMFCore.permissions import setDefaultRoles
 from zope.i18nmessageid import MessageFactory
 
 PROJECTNAME = "plone.app.event"
-messageFactory = MessageFactory(config.PROJECTNAME)
+messageFactory = MessageFactory(PROJECTNAME)
 
 ADD_PERMISSION = 'ATContentTypes: Add Event' # ATContentTypes permissions
 PORTAL_ADD_PERMISSION = 'Add portal events' # CMFCalendar permissions
