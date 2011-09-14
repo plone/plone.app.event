b12f7eb3a1f0cc0c23210be8eba7a35e51d39895
Johannes Raggam <raggam-nl@adm.at>
2011-09-14 12:46:17 +0200

    define IEvent attributes

--- plone/app/event/interfaces.py
+++ plone/app/event/interfaces.py
@@ -1,15 +1,68 @@
 from zope import schema
-from zope.interface import Interface
+from zope.interface import Interface, Attribute
 
-from plone.event.interfaces import IEvent as IBaseEvent
 from plone.event.utils import default_timezone as fallback_default_timezone
 from plone.app.event import messageFactory as _
 
 
-class IEvent(IBaseEvent):
+class IEvent(Interface):
+    """ Event schema
+
+    For reference: this are the RFC5545 event properties:
+
+    ; The following are REQUIRED,
+    ; but MUST NOT occur more than once.
+    ;
+    dtstamp / uid /
+    ;
+    ; The following is REQUIRED if the component
+    ; appears in an iCalendar object that doesn't
+    ; specify the "METHOD" property; otherwise, it
+    ; is OPTIONAL; in any case, it MUST NOT occur
+    ; more than once.
+    ;
+    dtstart /
+    ;
+    ; The following are OPTIONAL,
+    ; but MUST NOT occur more than once.
+    ;
+    class / created / description / geo /
+    last-mod / location / organizer / priority /
+    seq / status / summary / transp /
+    url / recurid /
+    ;
+    ; The following is OPTIONAL,
+    ; but SHOULD NOT occur more than once.
+    ;
+    rrule /
+    ;
+    ; Either 'dtend' or 'duration' MAY appear in
+    ; a 'eventprop', but 'dtend' and 'duration'
+    ; MUST NOT occur in the same 'eventprop'.
+    ;
+    dtend / duration /
+    ;
+    ; The following are OPTIONAL,
+    ; and MAY occur more than once.
+    ;
+    attach / attendee / categories / comment /
+    contact / exdate / rstatus / related /
+    resources / rdate / x-prop / iana-prop
     """
-    """
-    #TODO: define schema interface from event at schema
+
+    start = Attribute(u"Event start date")
+    end = Attribute(u"Event end date")
+    timezone = Attribute(u"Timezone of the event")
+    recurrence = Attribute(u"RFC5545 compatible recurrence definition")
+    whole_day = Attribute(u"Event lasts whole day")
+    location = Attribute(u"Location of the event")
+    text = Attribute(u"Summary of the event")
+    attendees = Attribute(u"List of attendees")
+    event_url = Attribute(u"Website of the event")
+    contact_name = Attribute(u"Contact name")
+    contact_email = Attribute(u"Contact email")
+    contact_phone = Attribute(u"Contact phone")
+
 
 
 class IEventSettings(Interface):
