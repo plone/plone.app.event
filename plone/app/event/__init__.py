c4bda1022f275af0e7fe65ff1b692311bf98cdef
Johannes Raggam <raggam-nl@adm.at>
2011-10-19 00:36:59 +0200

    remove portal_calendar

--- plone/app/event/__init__.py
+++ plone/app/event/__init__.py
@@ -11,11 +11,3 @@ PORTAL_CHANGE_PERMISSION = 'Change portal events'
 setDefaultRoles(ADD_PERMISSION, ('Manager', 'Site Administrator', 'Owner',))
 setDefaultRoles(PORTAL_ADD_PERMISSION, ('Manager', 'Site Administrator', 'Owner',))
 setDefaultRoles(PORTAL_CHANGE_PERMISSION, ('Manager', 'Site Administrator', 'Owner',))
-
-def initialize(context):
-    from plone.app.event.calendar_tool import CalendarTool
-    from Products.CMFPlone.utils import ToolInit
-    ToolInit('Plone Tool',
-             tools=[CalendarTool],
-             icon='tool.gif',
-             ).initialize(context)
