b6d883cb54916e1099c717d0e8a187b5a8ecd404
Johannes Raggam <raggam-nl@adm.at>
2011-12-08 16:10:28 +0100

    rfc5545 desc removed

--- plone/app/event/interfaces.py
+++ plone/app/event/interfaces.py
@@ -6,50 +6,8 @@ from plone.app.event import messageFactory as _
 
 
 class IEvent(Interface):
-    """ Event schema
-
-    For reference: this are the RFC5545 event properties:
-
-    ; The following are REQUIRED,
-    ; but MUST NOT occur more than once.
-    ;
-    dtstamp / uid /
-    ;
-    ; The following is REQUIRED if the component
-    ; appears in an iCalendar object that doesn't
-    ; specify the "METHOD" property; otherwise, it
-    ; is OPTIONAL; in any case, it MUST NOT occur
-    ; more than once.
-    ;
-    dtstart /
-    ;
-    ; The following are OPTIONAL,
-    ; but MUST NOT occur more than once.
-    ;
-    class / created / description / geo /
-    last-mod / location / organizer / priority /
-    seq / status / summary / transp /
-    url / recurid /
-    ;
-    ; The following is OPTIONAL,
-    ; but SHOULD NOT occur more than once.
-    ;
-    rrule /
-    ;
-    ; Either 'dtend' or 'duration' MAY appear in
-    ; a 'eventprop', but 'dtend' and 'duration'
-    ; MUST NOT occur in the same 'eventprop'.
-    ;
-    dtend / duration /
-    ;
-    ; The following are OPTIONAL,
-    ; and MAY occur more than once.
-    ;
-    attach / attendee / categories / comment /
-    contact / exdate / rstatus / related /
-    resources / rdate / x-prop / iana-prop
+    """ Generic Event
     """
-
     start = Attribute(u"Event start date")
     end = Attribute(u"Event end date")
     timezone = Attribute(u"Timezone of the event")
