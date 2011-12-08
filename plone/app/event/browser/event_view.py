3365c7641e90b28e1519eef35a2a7918abf197d7
Johannes Raggam <raggam-nl@adm.at>
2011-12-08 15:49:11 +0100

    refactor toDisplay to prepare_for_display to not be bound to the ATEvent schema. create AT and DX browser views (soon to be unified)

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,16 +1,20 @@
 from Products.Five.browser import BrowserView
 from Products.CMFPlone.i18nl10n import ulocalized_time
-from plone.event.utils import isSameDay, isSameTime
+from plone.event.utils import is_same_day, is_same_time
+from plone.app.event.base import DT
+from plone.app.event.interfaces import IRecurrence
 
 
-def toDisplay(event):
-    """ Return dict containing pre-calculated information for
-        building a <start>-<end> date string. Keys are
-       'start_date' - date string of the start date
-       'start_time' - time string of the start date
-       'end_date' - date string of the end date
-       'end_time' - time string of the end date
-       'same_day' - event ends on the same day
+def prepare_for_display(context, start, end, whole_day):
+    """ Return a dictionary containing pre-calculated information for building
+    <start>-<end> date strings.
+
+    Keys are:
+        'start_date' - date string of the start date
+        'start_time' - time string of the start date
+        'end_date'   - date string of the end date
+        'end_time'   - time string of the end date
+        'same_day'   - event ends on the same day
     """
 
     # The behavior os ulocalized_time() with time_only is odd.
@@ -25,19 +29,21 @@ def toDisplay(event):
     # u'16.03.2010'
 
     # this needs to separate date and time as ulocalized_time does
-    start_date = ulocalized_time(event.start(), False,
-                                 time_only=None, context=event)
-    start_time = ulocalized_time(event.start(), False,
-                                 time_only=True, context=event)
-    end_date = ulocalized_time(event.end(), False,
-                               time_only=None, context=event)
-    end_time = ulocalized_time(event.end(), False,
-                               time_only=True, context=event)
-    same_day = isSameDay(event)
-    same_time = isSameTime(event)
+    DT_start = DT(start)
+    DT_end = DT(end)
+    start_date = ulocalized_time(DT_start, long_format=False, time_only=None,
+                                 context=context)
+    start_time = ulocalized_time(DT_start, long_format=False, time_only=True,
+                                 context=context)
+    end_date = ulocalized_time(DT_end, long_format=False, time_only=None,
+                               context=context)
+    end_time = ulocalized_time(DT_end, long_format=False, time_only=True,
+                               context=context)
+    same_day = is_same_day(start, end)
+    same_time = is_same_time(start, end)
 
     # set time fields to None for whole day events
-    if event.whole_day:
+    if whole_day:
         start_time = end_time = None
 
     # TODO convert start_date, start_time, end_date, end_time
@@ -47,32 +53,47 @@ def toDisplay(event):
                  start_time=start_time,
                  end_date=end_date,
                  end_time=end_time,
-                 start_iso=event.start().ISO8601(),
-                 end_iso=event.end().ISO8601(),
+                 start_iso=start.isoformat(),
+                 end_iso=end.isoformat(),
                  same_day=same_day,
                  same_time=same_time)
 
 
-class EventView(BrowserView):
+
+class DXEventView(BrowserView):
 
     def date_for_display(self):
-        return toDisplay(self.context)
+        return prepare_for_display(
+                self.context,
+                self.context.start,
+                self.context.end,
+                self.context.whole_day)
 
     @property
     def occurrences(self):
-        # TODO convert start_date, start_time, end_date, end_time
-        # to user or portal timezone. Don't convert iso.
         context = self.context
         events = map(
-            lambda occ: dict(
-                start_date = ulocalized_time(occ[0], False, time_only=None, context=context),
-                end_date = ulocalized_time(occ[1], False, time_only=None, context=context),
-                start_time = ulocalized_time(occ[0], False, time_only=True, context=context),
-                end_time = ulocalized_time(occ[1], False, time_only=True, context=context),
-                start_iso = occ[0].isoformat(),
-                end_iso = occ[1].isoformat(),
-                same_day = occ[0].date() == occ[1].date(),
-                same_time = occ[0].time() == occ[1].time(),
-            ), context.occurrences())
-
+            lambda start, end: prepare_for_display(self.context, start, end,
+                                                   self.context.whole_day),
+            IRecurrence(context).occurrences())
         return events
+
+
+class ATEventView(DXEventView):
+
+    def date_for_display(self):
+        return prepare_for_display(
+                self.context,
+                self.context.start_date,
+                self.context.end_date,
+                self.context.whole_day)
+
+    @property
+    def occurrences(self):
+        context = self.context
+        events = map(
+            lambda start, end: prepare_for_display(self.context, start, end,
+                                                   self.context.whole_day),
+            IRecurrence(context).occurrences())
+        return events
+
