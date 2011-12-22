27ce2e2aa526865b2772218877fb3844113451aa
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-22 14:52:35 +0100

    add i18n.py to automaticly translate some messageids

---
+++ plone/app/event/i18n.py
@@ -0,0 +1,9 @@
+from plone.app.event import messageFactory as _
+
+_('weekday-abbrev-0')
+_('weekday-abbrev-1')
+_('weekday-abbrev-2')
+_('weekday-abbrev-3')
+_('weekday-abbrev-4')
+_('weekday-abbrev-5')
+_('weekday-abbrev-6')
