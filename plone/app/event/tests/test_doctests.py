ab18a01c27b7a0d786ba86ed76cc15626c40e800
Johannes Raggam <raggam-nl@adm.at>
2012-04-11 14:13:36 +0200

    update docs

--- plone/app/event/tests/test_doctests.py
+++ plone/app/event/tests/test_doctests.py
@@ -1,15 +1,30 @@
 import unittest2 as unittest
 import doctest
+import os.path
+from interlude import interact
 
+OPTIONFLAGS = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 DOCFILES = [
     'webdav.txt'
 ]
+DOCMODS = [
+]
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         doctest.DocFileSuite(
-            docfile,
-            optionflags=doctest.ELLIPSIS,
+            os.path.join(os.path.dirname(__file__), docfile),
+            module_relative=False,
+            optionflags=OPTIONFLAGS,
+            globs={'interact': interact}
         ) for docfile in DOCFILES
+    ])
+    suite.addTests([
+        doctest.DocTestSuite(
+            docmod,
+            optionflags=OPTIONFLAGS,
+            globs={'interact': interact}
+        ) for docmod in DOCMODS
     ])
     return suite
