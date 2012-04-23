e928b68f1b29655d0583876ab69582613cecdccf
Johannes Raggam <raggam-nl@adm.at>
2012-04-23 21:54:32 +0200

    add tests for daterecurringindex indizes from P.CMFCore.

---
+++ plone/app/event/tests/test_catalog.py
@@ -0,0 +1,20 @@
+import unittest2 as unittest
+from Products.CMFCore.utils import getToolByName
+from plone.app.event.testing import PAEvent_INTEGRATION_TESTING
+
+class TextDXIntegration(unittest.TestCase):
+    layer = PAEvent_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.catalog = getToolByName(self.portal, 'portal_catalog')
+
+    def test_end_is_DateRecurringIndex(self):
+        # end should be a DateRecurringIndex
+        self.assertEqual(self.catalog.Indexes['end'].__class__.__name__,
+                         'DateRecurringIndex')
+
+    def test_start_is_DateRecurringIndex(self):
+        # start should be a DateRecurringIndex
+        self.assertEqual(self.catalog.Indexes['start'].__class__.__name__,
+                         'DateRecurringIndex')
