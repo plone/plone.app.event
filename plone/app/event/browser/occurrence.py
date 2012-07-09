054925f8df3d908fb9305cc785c4b024e81fc922
Roman Joost <roman@mooball.com>
2012-07-09 14:14:11 +1000

    Show only occurrences concerning this day.

--- plone/app/event/browser/occurrence.py
+++ plone/app/event/browser/occurrence.py
@@ -9,10 +9,14 @@ class Occurrences(BrowserView):
 
     def get_data(self):
         start = guess_date_from(self.request.form.get('start', ''))
+        limit = self.request.form.get('limit', None)
         b_size = self.request.form.get('b_size', 20)
         b_start = self.request.form.get('b_start', 0)
         result = get_portal_events(self.context, range_start=start)
         return Batch(
-            get_occurrences(self.context, result, range_start=start),
+            get_occurrences(self.context,
+                            result,
+                            limit=limit,
+                            range_start=start),
             b_size,
             b_start)
