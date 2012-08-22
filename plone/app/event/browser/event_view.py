994224f5130e36c54046dd51024d0dd4b61221cb
Sean Upton <sdupton@gmail.com>
2012-08-22 11:42:03 -0600

    work around bug in DateTime.strftime that always normalizes displayed date to the system timezone, regardless of the timezone on the DateTime value itself.  This bug is not likely to get fixed in Zope DateTime upstream soon (long-standing), so Products.CMFPlone.i18nl10n.ulocalized_time is subject to triggering it -- here that function is wrapped with a local copy that does the right thing with the timezone by using datetime.datetime.strftime without broken magic of DateTime.strftime.

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,9 +1,8 @@
-from Products.CMFPlone.i18nl10n import ulocalized_time
 from Products.Five.browser import BrowserView
 from plone.event.interfaces import IEventAccessor, IRecurrenceSupport
 from plone.event.utils import is_same_day, is_same_time
 
-from plone.app.event.base import DT
+from plone.app.event.base import DT, ulocalized_time
 
 
 def prepare_for_display(context, start, end, whole_day):
