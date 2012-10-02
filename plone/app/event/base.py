03419ae6034b04467bba786c3f8ef98dfee82231
Johannes Raggam <raggam-nl@adm.at>
2012-10-03 01:57:39 +0200

    move prepare_for_display from event_view to base. should be reused also for other views. move in format_event_dates from g24.elements to replace the ZPT logic in the event_view template and to be reused for other templates.

--- plone/app/event/base.py
+++ plone/app/event/base.py
@@ -46,7 +46,7 @@ class CalendarLinkbase(object):
         :param date: The date to search events for in isoformat (ISO 8601,
                     YYYY-MM-DD).
         :type date: string
-        
+
         :param path: Events context path (optional).
         :type path: string
 
@@ -459,6 +459,84 @@ def guess_date_from(datestr, context=None):
         return
 
     return pytz.timezone(default_timezone(context)).localize(dateobj)
+
+
+def format_event_dates(context, start, end, whole_day=False):
+
+    formated_dates = prepare_for_display(context, start, end, whole_day)
+
+    if formated_dates['same_day'] and whole_day:
+        return '%s' % formated_dates['start_date']
+    elif formated_dates['same_day'] and formated_dates['same_time']:
+        return '%s %s' % (formated_dates['start_date'],
+                          formated_dates['start_time'])
+    elif formated_dates['same_day']:
+        return '%s %s - %s' % (formated_dates['start_date'],
+                               formated_dates['start_time'],
+                               formated_dates['end_time'])
+    elif whole_day:
+        return '%s - %s' % (formated_dates['start_date'],
+                            formated_dates['end_date'])
+    else:
+        return '%s %s - %s %s' % (formated_dates['start_date'],
+                                  formated_dates['start_time'],
+                                  formated_dates['end_time'],
+                                  formated_dates['end_time'])
+
+
+from plone.event.utils import is_same_day, is_same_time
+def prepare_for_display(context, start, end, whole_day):
+    """ Return a dictionary containing pre-calculated information for building
+    <start>-<end> date strings.
+
+    Keys are:
+        'start_date' - date string of the start date
+        'start_time' - time string of the start date
+        'end_date'   - date string of the end date
+        'end_time'   - time string of the end date
+        'start_iso'  - start date in iso format
+        'end_iso'    - end date in iso format
+        'same_day'   - event ends on the same day
+        'same_time'  - event ends at same time
+    """
+
+    # The behavior os ulocalized_time() with time_only is odd.
+    # Setting time_only=False should return the date part only and *not*
+    # the time
+    #
+    # ulocalized_time(event.start(), False,  time_only=True, context=event)
+    # u'14:40'
+    # ulocalized_time(event.start(), False,  time_only=False, context=event)
+    # u'14:40'
+    # ulocalized_time(event.start(), False,  time_only=None, context=event)
+    # u'16.03.2010'
+
+    # this needs to separate date and time as ulocalized_time does
+    DT_start = DT(start)
+    DT_end = DT(end)
+    start_date = ulocalized_time(DT_start, long_format=False, time_only=None,
+                                 context=context)
+    start_time = ulocalized_time(DT_start, long_format=False, time_only=True,
+                                 context=context)
+    end_date = ulocalized_time(DT_end, long_format=False, time_only=None,
+                               context=context)
+    end_time = ulocalized_time(DT_end, long_format=False, time_only=True,
+                               context=context)
+    same_day = is_same_day(start, end)
+    same_time = is_same_time(start, end)
+
+    # set time fields to None for whole day events
+    if whole_day:
+        start_time = end_time = None
+
+    return dict(start_date=start_date,
+                 start_time=start_time,
+                 start_iso=start.isoformat(),
+                 end_date=end_date,
+                 end_time=end_time,
+                 end_iso=end.isoformat(),
+                 same_day=same_day,
+                 same_time=same_time)
 
 
 # Workaround for buggy strftime with timezone handling in DateTime.
--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -4,63 +4,7 @@ from plone.event.interfaces import IEventAccessor
 from plone.event.interfaces import IRecurrenceSupport
 from plone.event.interfaces import IOccurrence
 
-from plone.event.utils import is_same_day, is_same_time
-
-from plone.app.event.base import DT, ulocalized_time
-
-
-def prepare_for_display(context, start, end, whole_day):
-    """ Return a dictionary containing pre-calculated information for building
-    <start>-<end> date strings.
-
-    Keys are:
-        'start_date' - date string of the start date
-        'start_time' - time string of the start date
-        'end_date'   - date string of the end date
-        'end_time'   - time string of the end date
-        'start_iso'  - start date in iso format
-        'end_iso'    - end date in iso format
-        'same_day'   - event ends on the same day
-        'same_time'  - event ends at same time
-    """
-
-    # The behavior os ulocalized_time() with time_only is odd.
-    # Setting time_only=False should return the date part only and *not*
-    # the time
-    #
-    # ulocalized_time(event.start(), False,  time_only=True, context=event)
-    # u'14:40'
-    # ulocalized_time(event.start(), False,  time_only=False, context=event)
-    # u'14:40'
-    # ulocalized_time(event.start(), False,  time_only=None, context=event)
-    # u'16.03.2010'
-
-    # this needs to separate date and time as ulocalized_time does
-    DT_start = DT(start)
-    DT_end = DT(end)
-    start_date = ulocalized_time(DT_start, long_format=False, time_only=None,
-                                 context=context)
-    start_time = ulocalized_time(DT_start, long_format=False, time_only=True,
-                                 context=context)
-    end_date = ulocalized_time(DT_end, long_format=False, time_only=None,
-                               context=context)
-    end_time = ulocalized_time(DT_end, long_format=False, time_only=True,
-                               context=context)
-    same_day = is_same_day(start, end)
-    same_time = is_same_time(start, end)
-
-    # set time fields to None for whole day events
-    if whole_day:
-        start_time = end_time = None
-
-    return dict(start_date=start_date,
-                 start_time=start_time,
-                 start_iso=start.isoformat(),
-                 end_date=end_date,
-                 end_time=end_time,
-                 end_iso=end.isoformat(),
-                 same_day=same_day,
-                 same_time=same_time)
+from plone.app.event.base import prepare_for_display
 
 
 class EventView(BrowserView):
