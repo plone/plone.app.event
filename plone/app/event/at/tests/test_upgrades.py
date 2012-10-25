0a27b136dffd33e2f0d2aadd1034dee6752a90d3
Johannes Raggam <raggam-nl@adm.at>
2012-10-25 13:46:03 +0200

    fixed upgrade test

--- plone/app/event/at/tests/test_upgrades.py
+++ plone/app/event/at/tests/test_upgrades.py
@@ -11,28 +11,24 @@ from plone.app.testing import setRoles
 from plone.event.interfaces import IEvent
 import unittest2 as unittest
 import zope.interface
-from plone.app.event.at.content import ATEvent
+from plone.app.event.at.content import ATEvent, ATEventSchema
 
-MockSchema = ATContentTypeSchema.copy() + atapi.Schema((
-    atapi.DateTimeField('startDate',
-        accessor='start',
-        default_method=DateTime,
-        widget=atapi.StringWidget(),
-    ),
-    atapi.DateTimeField('endDate',
-        accessor='end',
-        default_method=DateTime,
-        widget=atapi.StringWidget(),
-    ),
+# For Schema upgrade test, copy the ATEventSchema and add timezone and 
+# recurrence field with AnnotationStorage
+MockSchema = ATEventSchema.copy()
+MockSchema.addField(
     atapi.StringField('timezone',
         storage=atapi.AnnotationStorage(),
         widget=atapi.StringWidget(),
-        ),
+    )
+)
+MockSchema.addField(
     atapi.StringField('recurrence',
         storage=atapi.AnnotationStorage(),
         widget=atapi.StringWidget(),
-    ),
-))
+    )
+)
+
 
 class MockATEvent_1(BaseContent):
     zope.interface.implements(IATEvent)
@@ -60,15 +56,15 @@ class PAEventATMigrationTest(unittest.TestCase):
         self._dummy_1.title = 'Foo'
         self._dummy_1.reindexObject()
 
-        #self._dummy_2 = mkDummyInContext(MockATEvent_2, oid='dummy_2',
-        #                               context=self.portal,
-        #                               schema=MockSchema)
-        #self._dummy_2.title = 'Baz'
-        #self._dummy_2.setStartDate(DateTime())
-        #self._dummy_2.setEndDate(DateTime())
-        #self._dummy_2.setTimezone("Europe/Vienna")
-        #self._dummy_2.setRecurrence("RRULE:FREQ=DAILY;COUNT=3")
-        #self._dummy_2.reindexObject()
+        self._dummy_2 = mkDummyInContext(MockATEvent_2, oid='dummy_2',
+                                       context=self.portal,
+                                       schema=MockSchema)
+        self._dummy_2.title = 'Baz'
+        self._dummy_2.setStartDate(DateTime())
+        self._dummy_2.setEndDate(DateTime())
+        self._dummy_2.setTimezone("Europe/Vienna")
+        self._dummy_2.setRecurrence("RRULE:FREQ=DAILY;COUNT=3")
+        self._dummy_2.reindexObject()
 
     def test_upgrade_step_1(self):
         upgrade_step_1(self.portal)
@@ -77,16 +73,46 @@ class PAEventATMigrationTest(unittest.TestCase):
         self.assertEqual('', event.recurrence)
         self.assertEqual('Foo', event.Title())
 
-#    def test_upgrade_step_2(self):
-#
-#        upgrade_step_2(self.portal)
-#
-#        event = self.portal['dummy_2']
-#        tz_field = event.getField('timezone')
-#        re_field = event.getField('recurrence')
-#        self.assertTrue(isinstance(tz_field.storage, atapi.AttributeStorage))
-#        self.assertTrue(tz_field.getLayerImpl('storage') ==
-#                        atapi.AttributeStorage())
-#        self.assertTrue(isinstance(re_field.storage, atapi.AttributeStorage))
-#        self.assertTrue(re_field.getLayerImpl('storage') ==
-#                        atapi.AttributeStorage())
+    def test_upgrade_step_2(self):
+
+        event = self.portal['dummy_2']
+
+        # Old schema. Values as expected, storage is AnnotationStorage
+        self.assertTrue(event.getTimezone() == 'Europe/Vienna')
+        self.assertTrue(event.getRecurrence() == 'RRULE:FREQ=DAILY;COUNT=3')
+        self.assertTrue(isinstance(event.getField('timezone').storage,
+                                   atapi.AnnotationStorage))
+        self.assertTrue(isinstance(event.getField('recurrence').storage,
+                                   atapi.AnnotationStorage))
+
+        schema = event.schema
+        schema.addField(
+            atapi.StringField('timezone',
+                widget=atapi.StringWidget(),
+            )
+        )
+        schema.addField(
+            atapi.StringField('recurrence',
+                widget=atapi.StringWidget(),
+            )
+        )
+
+        # New schema before upgrade. Empty fields, storage is AttributeStorage
+        self.assertTrue(event.getTimezone() == '')
+        self.assertTrue(event.getRecurrence() == '')
+        self.assertTrue(isinstance(event.getField('timezone').storage,
+                                   atapi.AttributeStorage))
+        self.assertTrue(isinstance(event.getField('recurrence').storage,
+                                   atapi.AttributeStorage))
+
+        # Upgrade!
+        upgrade_step_2(self.portal)
+
+        # New schema after upgrade. Values as expected, storage is
+        # AttributeStorage
+        self.assertTrue(event.getTimezone() == 'Europe/Vienna')
+        self.assertTrue(event.getRecurrence() == 'RRULE:FREQ=DAILY;COUNT=3')
+        self.assertTrue(isinstance(event.getField('timezone').storage,
+                                   atapi.AttributeStorage))
+        self.assertTrue(isinstance(event.getField('recurrence').storage,
+                                   atapi.AttributeStorage))
