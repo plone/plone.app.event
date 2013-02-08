b8b4d7c8577a63b7b9fd84eb430b5848af827951
Johannes Raggam <raggam-nl@adm.at>
2013-02-08 15:29:02 +0100

    add basic i18ndude wrapper script

---
+++ build-i18n.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+I18NDUDE=../../bin/i18ndude
+$I18NDUDE rebuild-pot --pot plone/app/event/locales/plone.app.event.pot --create plone.app.event .
+$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/de/LC_MESSAGES/plone.app.event.po 
+$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/en/LC_MESSAGES/plone.app.event.po 
+$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/fr/LC_MESSAGES/plone.app.event.po 
+$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/it/LC_MESSAGES/plone.app.event.po 
