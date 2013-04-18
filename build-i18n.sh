8353827afe05ed68bb866c8d6cbd809df476e66d
Johannes Raggam <raggam-nl@adm.at>
2013-04-18 16:59:03 +0200

    update build i18n and include plone domain

--- build-i18n.sh
+++ build-i18n.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 I18NDUDE=../../bin/i18ndude
-$I18NDUDE rebuild-pot --pot plone/app/event/locales/plone.app.event.pot --create plone.app.event .
-$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/de/LC_MESSAGES/plone.app.event.po 
-$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/en/LC_MESSAGES/plone.app.event.po 
-$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/fr/LC_MESSAGES/plone.app.event.po 
-$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/it/LC_MESSAGES/plone.app.event.po 
-$I18NDUDE sync --pot plone/app/event/locales/plone.app.event.pot plone/app/event/locales/pt_BR/LC_MESSAGES/plone.app.event.po 
+I18NPATH=plone/app/event
+DOMAIN=plone.app.event
+$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
+$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
+
+DOMAIN=plone
+$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --merge $I18NPATH/locales/merge-plone.pot --create $DOMAIN $I18NPATH
+$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
