5d1d03f3c2fded435142257216b14bd53ca8f0a9
Johannes Raggam <raggam-nl@adm.at>
2013-04-30 13:02:36 +0200

    only necessary msgids in plone overwrite

--- build-i18n.sh
+++ build-i18n.sh
@@ -3,8 +3,10 @@ I18NDUDE=../../bin/i18ndude
 I18NPATH=plone/app/event
 DOMAIN=plone.app.event
 $I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
-$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
+$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
 
-DOMAIN=plone
-$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --merge $I18NPATH/locales/merge-plone.pot --create $DOMAIN $I18NPATH
-$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
+## Don't automatically run the rebuild for the "plone" domain, as it puts too
+## much message strings into it.
+#DOMAIN=plone
+#$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --merge $I18NPATH/locales/merge-plone.pot --create $DOMAIN $I18NPATH
+#$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
