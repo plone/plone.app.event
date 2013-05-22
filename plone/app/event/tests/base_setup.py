1df4b65f70b0508e24fa260b33d7a183b4530282
Johannes Raggam <raggam-nl@adm.at>
2013-05-22 09:38:22 +0200

    Patch localized_now to allow stable results in tests.

--- plone/app/event/tests/base_setup.py
+++ plone/app/event/tests/base_setup.py
@@ -1,29 +1,52 @@
-from plone.app.event.base import default_timezone
+from plone.app.event import base
 from plone.app.event.base import localized_now
 from plone.app.event.interfaces import IEventSettings
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 from plone.registry.interfaces import IRegistry
 
-import datetime
+from datetime import datetime
+from datetime import timedelta
 import unittest2 as unittest
 import zope.component
+
+ORIG_NOW = localized_now
+
+
+def patched_now(context=None):
+    """Patch localized_now to allow stable results in tests.
+    """
+    if not context:
+        context = None
+    tzinfo = base.default_timezone(context=context, as_tzinfo=True)
+    now = datetime(2013, 5, 5, 10, 0, 0, tzinfo=tzinfo).replace(microsecond=0)
+    return now
 
 
 class AbstractSampleDataEvents(unittest.TestCase):
     layer = None  # Set the plone.app.testing layer in concrete implementation
+
+    def __init__(self, *args, **kwargs):
+        super(AbstractSampleDataEvents, self).__init__(*args, **kwargs)
+        # Patch localized_now
+        self._orig_now = base.localized_now
+        base.localized_now = patched_now
 
     def event_factory(self):
         # Return the IEventAccessor.create event factory.
         raise NotImplementedError
 
     def make_dates(self):
-        now      = self.now      = localized_now()
-        past     = self.past     = now - datetime.timedelta(days=10)
-        future   = self.future   = now + datetime.timedelta(days=10)
-        far      = self.far      = now + datetime.timedelta(days=30)
-        duration = self.duration = datetime.timedelta(hours=1)
+        now      = self.now      = base.localized_now()
+        past     = self.past     = now - timedelta(days=10)
+        future   = self.future   = now + timedelta(days=10)
+        far      = self.far      = now + timedelta(days=30)
+        duration = self.duration = timedelta(hours=1)
         return (now, past, future, far, duration)
+
+    def tearDown(self):
+        # Unpatch localized_now
+        base.localized_now = ORIG_NOW
 
     def setUp(self):
         self.portal = self.layer['portal']
@@ -32,7 +55,7 @@ class AbstractSampleDataEvents(unittest.TestCase):
 
         reg = zope.component.getUtility(IRegistry)
         settings = reg.forInterface(IEventSettings, prefix="plone.app.event")
-        default_tz = default_timezone()
+        default_tz = base.default_timezone()
         settings.portal_timezone = default_tz
 
         now, past, future, far, duration = self.make_dates()
@@ -79,3 +102,4 @@ class AbstractSampleDataEvents(unittest.TestCase):
             end=far,
             location=u'Schaftal',
             timezone=default_tz)
+
