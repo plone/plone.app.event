5f319091ab1c08b154ca01568144264baea94979
Wolfgang Thomas <thomas@syslab.com>
2014-02-11 14:21:55 +0100

    BBB for the old marker interfaces for Location, Attendees and Contact

--- plone/app/event/dx/interfaces.py
+++ plone/app/event/dx/interfaces.py
@@ -1,6 +1,8 @@
+from plone.app.event.dx.behaviors import IEventAttendees
+from plone.app.event.dx.behaviors import IEventContact
+from plone.app.event.dx.behaviors import IEventLocation
 from plone.event.interfaces import IEvent
 from plone.event.interfaces import IEventRecurrence
-from zope.interface import Interface
 
 
 class IDXEvent(IEvent):
@@ -13,16 +15,16 @@ class IDXEventRecurrence(IEventRecurrence):
     """
 
 
-class IDXEventLocation(Interface):
+class IDXEventLocation(IEventLocation):
     """ Marker interface for Dexterity events with location.
     """
 
 
-class IDXEventAttendees(Interface):
+class IDXEventAttendees(IEventAttendees):
     """ Marker interface for Dexterity events with attendees.
     """
 
 
-class IDXEventContact(Interface):
+class IDXEventContact(IEventContact):
     """ Marker interface for Dexterity events with contact information.
     """
