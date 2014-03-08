eebb59c3a91f7a015af325e42b2ea2e82d13bc6f
David Glick <david@glicksoftware.com>
2014-03-07 22:44:46 -0800

    convert old path values

--- plone/app/event/portlets/portlet_calendar.py
+++ plone/app/event/portlets/portlet_calendar.py
@@ -1,4 +1,5 @@
 from Acquisition import aq_inner
+from ComputedAttribute import ComputedAttribute
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.event.base import RET_MODE_OBJECTS
@@ -9,9 +10,11 @@ from plone.app.event.base import wkday_to_mon1
 from plone.app.event.portlets import get_calendar_url
 from plone.app.portlets import PloneMessageFactory as _
 from plone.app.portlets.portlets import base
+from plone.app.uuid.utils import uuidToObject
 from plone.app.vocabularies.catalog import CatalogSource
 from plone.event.interfaces import IEventAccessor
 from plone.portlets.interfaces import IPortletDataProvider
+from zExceptions import NotFound
 from zope import schema
 from zope.i18nmessageid import MessageFactory
 from zope.interface import implements
@@ -35,7 +38,7 @@ class ICalendarPortlet(IPortletDataProvider):
             vocabulary="plone.app.vocabularies.WorkflowStates")
     )
 
-    search_base = schema.Choice(
+    search_base_uid = schema.Choice(
         title=_(u'portlet_label_search_base', default=u'Search base'),
         description=_(
             u'portlet_help_search_base',
@@ -57,23 +60,38 @@ class Assignment(base.Assignment):
     state = None
     search_base = None
 
-    def __init__(self, state=None, search_base=None):
+    def __init__(self, state=None, search_base_uid=None):
         self.state = state
-        self.search_base = search_base
+        self.search_base_uid = search_base_uid
+
+    def _uid(self):
+        # This is only called if the instance doesn't have a uid
+        # attribute, which is probably because it has an old
+        # 'target_collection' attribute that needs to be converted.
+        path = self.search_base
+        portal = getToolByName(self, 'portal_url').getPortalObject()
+        try:
+            search_base = portal.unrestrictedTraverse(path.lstrip('/'))
+        except (AttributeError, KeyError, TypeError, NotFound):
+            return
+        return search_base.UID()
+    search_base_uid = ComputedAttribute(_uid, 1)
 
 
 class Renderer(base.Renderer):
     render = ViewPageTemplateFile('portlet_calendar.pt')
 
+    def search_base_path(self):
+        search_base = uuidToObject(self.data.search_base_uid)
+        if search_base is not None:
+            search_base = '/'.join(search_base.getPhysicalPath())
+        return search_base
+
     def update(self):
         context = aq_inner(self.context)
 
-        search_base = None
-        if self.data.search_base:
-            search_base = self.data.search_base.to_path
-
         self.calendar_url = get_calendar_url(
-            context, search_base
+            context, self.search_base_path()
         )
 
         self.year, self.month = year, month = self.year_month_display()
@@ -157,8 +175,9 @@ class Renderer(base.Renderer):
         data = self.data
         query_kw = {}
 
-        if data.search_base:
-            query_kw['path'] = {'query': data.search_base.to_path}
+        search_base_path = self.search_base_path()
+        if search_base_path:
+            query_kw['path'] = {'query': search_base_path}
 
         if data.state:
             query_kw['review_state'] = data.state
@@ -219,7 +238,7 @@ class AddForm(base.AddForm):
 
     def create(self, data):
         return Assignment(state=data.get('state', None),
-                          search_base=data.get('search_base', None))
+                          search_base_uid=data.get('search_base_uid', None))
 
 
 class EditForm(base.EditForm):
