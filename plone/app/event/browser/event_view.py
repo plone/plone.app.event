959a45632ae0a6a8c8a710ed45760892a48f70bf
Johannes Raggam <raggam-nl@adm.at>
2014-04-10 16:02:15 +0200

    only show the statusmessage, if the user has rights to edit the event

--- plone/app/event/browser/event_view.py
+++ plone/app/event/browser/event_view.py
@@ -1,9 +1,11 @@
+from AccessControl import getSecurityManager
 from Acquisition import aq_parent
+from Products.CMFCore import permissions
 from Products.Five.browser import BrowserView
-from plone.event.interfaces import IEventAccessor
 from Products.statusmessages.interfaces import IStatusMessage
-from plone.event.interfaces import IOccurrence
 from plone.app.event import messageFactory as _
+from plone.event.interfaces import IEventAccessor
+from plone.event.interfaces import IOccurrence
 
 
 def get_location(accessor):
@@ -30,10 +32,23 @@ class EventView(BrowserView):
         self.request = request
         self.data = IEventAccessor(context)
 
-        if IOccurrence.providedBy(context):
-            msg = _(
-                'part_of_recurring_event',
-                default=u'This event is part of a recurring Event. '
-                        u'To edit the original event, go one level up.'
-            )
-            IStatusMessage(self.request).addStatusMessage(msg, 'info')
+    def __call__(self):
+        if IOccurrence.providedBy(self.context):
+            # If the context is a Occurrence, and the user has edit rights,
+            # tell the user to go onle level up to be able to edit the event
+            # via a IStatusMessage.
+            # Actually, a HTML link would be better, but that's not supported
+            # by the globalstatusmessage template at the moment
+            # (https://github.com/plone/Products.CMFPlone/issues/206).
+            event = aq_parent(self.context)
+            sm = getSecurityManager()
+            can_edit = sm.checkPermission(
+                permissions.ModifyPortalContent, event)
+            if can_edit:
+                msg = _(
+                    'part_of_recurring_event',
+                    default=u'This event is part of a recurring Event. '
+                            u'To edit the original event, go one level up.'
+                )
+                IStatusMessage(self.request).addStatusMessage(msg, 'info')
+        return self.index()  # render me.
