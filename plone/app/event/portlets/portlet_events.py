1625239ec225edbfe1e0bbb51de445774f1055d8
Rok Garbas <rok@garbas.si>
2015-07-20 17:35:20 +0200

    removing dependency on p.a.contenttypes

--- plone/app/event/portlets/portlet_events.py
+++ plone/app/event/portlets/portlet_events.py
@@ -2,8 +2,6 @@ from Acquisition import aq_inner
 from ComputedAttribute import ComputedAttribute
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.contenttypes.behaviors.collection import ISyndicatableCollection
-from plone.app.contenttypes.interfaces import IFolder
 from plone.app.event.base import expand_events
 from plone.app.event.base import _prepare_range
 from plone.app.event.base import start_end_query
@@ -27,7 +25,16 @@ from plone.app.querystring import queryparser
 
 try:
     from plone.app.contenttypes.behaviors.collection import ISyndicatableCollection as ICollection  # noqa
+    from plone.app.contenttypes.interfaces import IFolder
+    search_base_uid_source = CatalogSource(object_provides={
+        'query': [
+            ICollection.__identifier__,
+            IFolder.__identifier__
+        ],
+        'operator': 'or'
+    })
 except ImportError:
+    search_base_uid_source = CatalogSource(is_folderish=True)
     ICollection = None
 
 
@@ -61,13 +68,7 @@ class IEventsPortlet(IPortletDataProvider):
                     u'called on the site root.'
         ),
         required=False,
-        source=CatalogSource(object_provides={
-            'query': [
-                ISyndicatableCollection.__identifier__,
-                IFolder.__identifier__
-            ],
-            'operator': 'or'
-        }),
+        source=search_base_uid_source,
     )
 
 
@@ -155,7 +156,7 @@ class Renderer(base.Renderer):
         events = []
         query.update(self.request.get('contentFilter', {}))
         search_base = self.search_base
-        if ICollection.providedBy(search_base):
+        if ICollection and ICollection.providedBy(search_base):
             # Whatever sorting is defined, we're overriding it.
             query = queryparser.parseFormquery(
                 search_base, search_base.query,
