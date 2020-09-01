6ba7e901e6fc3699f7756591c0c9eba859d20739
Maurits van Rees <maurits@vanrees.org>
2020-09-01 22:00:12 +0200

    Fixed deprecation warning for setDefaultRoles.

--- plone/app/event/__init__.py
+++ plone/app/event/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Products.CMFCore.permissions import setDefaultRoles
+from AccessControl.Permission import addPermission
 from zope.i18nmessageid import MessageFactory
 
 packageName = __name__
@@ -8,7 +8,7 @@ _ = MessageFactory('plone')
 # BBB Permissions
 PORTAL_ADD_PERMISSION = 'Add portal events'  # CMFCalendar/ATCT permissions
 
-setDefaultRoles(
+addPermission(
     PORTAL_ADD_PERMISSION,
     ('Manager', 'Site Administrator', 'Owner',)
 )
