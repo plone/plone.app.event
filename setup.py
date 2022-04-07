c68fe0541570b37a0f0a88df4c3937920237e802
Maurits van Rees <maurits@vanrees.org>
2022-04-07 23:33:11 +0200

    Cleanup classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
