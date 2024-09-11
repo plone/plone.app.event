6577c4ab8610979b089a99781737f665cd15c5d4
1letter <1letter@gmx.de>
2024-09-11 15:35:58 +0200

    lint

--- plone/app/event/tests/robot/variables.py
+++ plone/app/event/tests/robot/variables.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from datetime import timedelta
 
 
+# important for MONTHNAME
 NOW = datetime.now() + timedelta(days=1)
-END = NOW + timedelta(hours=1)
-
 EVENT_START_ISO = NOW.strftime("%Y-%m-%dT%H:00")
+
+END = NOW + timedelta(hours=1)
 EVENT_END_ISO = END.strftime("%Y-%m-%dT%H:00")
